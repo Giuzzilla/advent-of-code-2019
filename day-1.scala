2993c35f28e940a1173f293ca42a095ea668ace0
Giulio Mazzanti <gmazzanti@studentsforliberty.org>
2019-12-01 16:28:21 +0100

    Remove semicolons for better Scala styling

--- day-1.scala
+++ day-1.scala
@@ -3,7 +3,7 @@ import scala.annotation.tailrec
 
 object Day1 {
   @tailrec def secondStar(fuel: Int, aggr: Int = 0) : Int = {
-    val current_fuel : Int = fuel / 3 - 2;
+    val current_fuel : Int = fuel / 3 - 2
     if (current_fuel <= 0)
       aggr
     else
@@ -18,9 +18,9 @@ object Day1 {
         args(0)
     }
 
-    val list = Source.fromFile(path).getLines.toArray.map(_.toInt);
-    println("First answer: " + list.map(x => x / 3 - 2).sum);
-    println("Second answer: " + list.map(x => secondStar(x)).sum);
+    val list = Source.fromFile(path).getLines.toArray.map(_.toInt)
+    println("First answer: " + list.map(x => x / 3 - 2).sum)
+    println("Second answer: " + list.map(x => secondStar(x)).sum)
 
   }
 }
