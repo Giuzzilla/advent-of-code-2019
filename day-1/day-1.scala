0cdb59788faac689a85061513a43d44ed235e184
Giulio Mazzanti <gmazzanti@studentsforliberty.org>
2019-12-02 10:28:02 +0100

    Day 2 Scala soolution + restyle of first day scala

--- day-1/day-1.scala
+++ day-1/day-1.scala
@@ -2,8 +2,8 @@ import scala.io.Source
 import scala.annotation.tailrec
 
 object Day1 {
-  @tailrec def secondStar(fuel: Int, aggr: Int = 0) : Int = {
-    val current_fuel : Int = fuel / 3 - 2
+  @tailrec def secondStar(fuel: Int, aggr: Int = 0): Int = {
+    val current_fuel: Int = fuel / 3 - 2
     if (current_fuel <= 0)
       aggr
     else
@@ -11,14 +11,14 @@ object Day1 {
   }
 
   def main(args: Array[String]) {
-    val path : String = {
+    val path: String = {
       if (args.length == 0) 
         "./input.txt"
       else
         args(0)
     }
 
-    val list = Source.fromFile(path).getLines.toArray.map(_.toInt)
+    val list: Array[Int] = Source.fromFile(path).getLines.toArray.map(_.toInt)
     println("First answer: " + list.map(x => x / 3 - 2).sum)
     println("Second answer: " + list.map(x => secondStar(x)).sum)
 
---
+++ day-2/day-2.scala
@@ -0,0 +1,51 @@
+import scala.io.Source
+
+object Day2 {
+  
+  def firstStar(passedArr: Array[Int], newpos1: Int, newpos2: Int): Int = {
+    var arr: Array[Int] = passedArr.clone
+    arr(1) = newpos1
+    arr(2) = newpos2
+    var pos = 0
+    while (arr(pos) != 99) {
+      arr(pos) match {
+        case 1 => arr(arr(pos+3)) = arr(arr(pos+1)) + arr(arr(pos+2))
+        case 2 => arr(arr(pos+3)) = arr(arr(pos+1)) * arr(arr(pos+2))
+        case default => throw new Exception("Matched " + default)
+      }
+      pos += 4
+    }
+    arr(0)
+  }
+
+  def secondStar(arr: Array[Int], required: Int): Int = {
+    var matched = -1
+    for (i <- 0 to 98; j <- 0 to 98) {
+      if (firstStar(arr, i, j) == required)
+        matched = i*100 + j
+    }
+    matched
+  }
+
+  def main(args: Array[String]) {
+    if (args.length < 3) {
+      throw new Exception("Missing params")  
+    }
+
+    val newpos1: Int = args(0).toInt
+    val newpos2: Int = args(1).toInt
+    val required: Int = args(2).toInt
+    
+    val path: String = {
+      if (args.length == 3)
+        "./input.txt"
+      else
+        args(4)
+    }
+
+
+    val arr: Array[Int] = Source.fromFile("./input.txt").getLines.toList(0).split(",").map(_.toInt)
+    println("First answer: " + firstStar(arr, newpos1, newpos2))
+    println("Second answer: " + secondStar(arr, required)) 
+  }
+}
